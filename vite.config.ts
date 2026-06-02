1ca8d9feb816fe523f3989d9356b90fbea1e7086
Flatorte <167423736+Flatortee@users.noreply.github.com>
2026-06-02 04:30:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [
-          ['babel-plugin-react-compiler', {}],
-        ],
+        plugins: [react()]
       },
     }),
   ],
